3c9d999fbf760ed5b7c1e62563298e2e6d6c805b
dukemiller <dukemiller928@hotmail.com>
2017-04-11 03:40:41 -0700

    Add initial viewmodels + locator

---
+++ destiny-chat-client/ViewModelLocator.cs
@@ -0,0 +1,21 @@
+﻿using destiny_chat_client.ViewModels;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Ioc;
+using Microsoft.Practices.ServiceLocation;
+
+namespace destiny_chat_client
+{
+    public class ViewModelLocator : ViewModelBase
+    {
+        static ViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+            SimpleIoc.Default.Register<MainWindowViewModel>();
+            SimpleIoc.Default.Register<ChatViewModel>();
+        }
+
+        public static MainWindowViewModel Main => ServiceLocator.Current.GetInstance<MainWindowViewModel>();
+
+        public static ChatViewModel Chat => ServiceLocator.Current.GetInstance<ChatViewModel>();
+    }
+}
---
+++ destiny-chat-client/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,9 @@
+﻿using GalaSoft.MvvmLight;
+
+namespace destiny_chat_client.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        
+    }
+}
