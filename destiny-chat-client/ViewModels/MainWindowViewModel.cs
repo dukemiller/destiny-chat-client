b0a858d8504a57577df2b4110bdfa9333207f815
dukemiller <dukemiller928@hotmail.com>
2017-04-12 23:27:54 -0700

    Heavy mainwindowviewmodel logic addition
    The popup modal logic for switching between commands, command history on unimplemented chatbar, logging in and out, sending messages, a few other things

--- destiny-chat-client/ViewModels/MainWindowViewModel.cs
+++ destiny-chat-client/ViewModels/MainWindowViewModel.cs
@@ -20,13 +20,325 @@ namespace destiny_chat_client.ViewModels
             Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
                 "destiny_chat_client");
 
+        // Fields
 
-        public MainWindowViewModel()
+        private ISettingsRepository _settingsRepository;
+
+        private string _message;
+
+        private bool _loggedIn;
+
+        private readonly ChatViewModel _chatViewModel;
+
+        private bool _isShowingPopup;
+
+        private readonly List<string> _commandHistory = new List<string>();
+
+        private int _commandHistoryIndex;
+
+        private UserControl _popupView;
+
+        private IChatService _chatService;
+
+        private IEmoteRepository _emoteRepository;
+
+        private bool _isShowingUserlist;
+
+        private bool _isShowingSettings;
+
+        private bool _isShowingEmotes;
+
+        private bool _isShowingLogin;
+
+        // Constructor
+
+        public MainWindowViewModel(IChatService chatService,
+            ISettingsRepository settingsRepository,
+            IEmoteRepository emoteRepository)
         {
+            ChatService = chatService;
+            EmoteRepository = emoteRepository;
+            _chatViewModel = SimpleIoc.Default.GetInstance<ChatViewModel>();
+            SettingsRepository = settingsRepository;
+            SettingsRepository.PropertyChanged += async (sender, args) =>
+            {
+                await SettingsRepository.Save();
+                if (args.PropertyName.Equals("Sid") || args.PropertyName.Equals("RememberMe"))
+                    LoginCommand.RaiseCanExecuteChanged();
+            };
+
+            SendCommand = new RelayCommand(SendMessage);
+            LoginCommand = new RelayCommand(Login, () => SettingsRepository.Sid.Length > 0 || SettingsRepository.RememberMe.Length > 0);
+            LogoutCommand = new RelayCommand(Logout);
+            ClearCommand = new RelayCommand(() => Message = "");
+
+            SettingsCommand = new RelayCommand(() =>
+            {
+                if (!IsShowingSettings)
+                {
+                    PopupView = new Settings();
+                    IsShowingSettings = true;
+                    IsShowingPopup = true;
+                }
+
+                else
+                    IsShowingPopup = false;
+            });
+
+            UserlistCommand = new RelayCommand(() =>
+            {
+                PopupView = new Users();
+                IsShowingUserlist = true;
+                IsShowingPopup = true;
+            });
+
+            EmotesCommand = new RelayCommand(() =>
+            {
+                PopupView = new Emotes();
+                IsShowingEmotes = true;
+                IsShowingPopup = true;
+            });
+
+            ShowLoginCommand = new RelayCommand(() =>
+            {
+                PopupView = new Login();
+                IsShowingSettings = false;
+                IsShowingLogin = true;
+            });
+
+            UseEmoteCommand = new RelayCommand<Emote>(emote => Message += emote.Description() + " ");
+
+            RetrieveDetailsCommand = new RelayCommand(() => ChatService.FindDetails());
             TrayCommand = new RelayCommand(() => new Tray(SimpleIoc.Default.GetInstance<ISettingsRepository>()));
+            AutoCompleteCommand = new RelayCommand(AutoComplete);
+
+            SaveCommand = new RelayCommand(() =>
+            {
+                IsShowingSettings = false;
+                SettingsRepository.Save();
+            });
+
+            ClosePopupCommand = new RelayCommand(() => IsShowingPopup = false);
+
+            LoggedIn = SettingsRepository.LoggedIn;
+            PreviousMessageCommand = new RelayCommand(PreviousMessage);
+            NextMessageCommand = new RelayCommand(NextMessage);
         }
+
+        // Popup logic
+
+        public bool IsShowingPopup
+        {
+            get => _isShowingPopup;
+            set
+            {
+                Set(() => IsShowingPopup, ref _isShowingPopup, value);
+                if (!IsShowingPopup)
+                {
+                    IsShowingUserlist = false;
+                    IsShowingSettings = false;
+                    IsShowingEmotes = false;
+                    IsShowingLogin = false;
+                }
+            }
+        }
+
+        public bool IsShowingUserlist
+        {
+            get => _isShowingUserlist;
+            set => Set(() => IsShowingUserlist, ref _isShowingUserlist, value);
+        }
+
+        public bool IsShowingSettings
+        {
+            get => _isShowingSettings;
+            set => Set(() => IsShowingSettings, ref _isShowingSettings, value);
+        }
+
+        public bool IsShowingEmotes
+        {
+            get => _isShowingEmotes;
+            set => Set(() => IsShowingEmotes, ref _isShowingEmotes, value);
+        }
+
+        public bool IsShowingLogin
+        {
+            get => _isShowingLogin;
+            set => Set(() => IsShowingLogin, ref _isShowingLogin, value);
+        }
+
+        // Services as public properties
+
+        public IChatService ChatService
+        {
+            get => _chatService;
+            set => Set(() => ChatService, ref _chatService, value);
+        }
+
+        public IEmoteRepository EmoteRepository
+        {
+            get => _emoteRepository;
+            set => Set(() => EmoteRepository, ref _emoteRepository, value);
+        }
+
+        public ISettingsRepository SettingsRepository
+        {
+            get => _settingsRepository;
+            set => Set(() => SettingsRepository, ref _settingsRepository, value);
+        }
+
+        // Chatbar commands
+
+        public string Message
+        {
+            get => _message;
+            set => Set(() => Message, ref _message, value);
+        }
+
+        public bool LoggedIn
+        {
+            get => _loggedIn;
+            set
+            {
+                Set(() => LoggedIn, ref _loggedIn, value);
+                RaisePropertyChanged(nameof(ChatHint));
+            }
+        }
+
+        public UserControl PopupView
+        {
+            get => _popupView;
+            set => Set(() => PopupView, ref _popupView, value);
+        }
+
+        public string ChatHint => LoggedIn ? "Enter a message..." : "Log in to type in chat.";
+
+        // Commands
+
+        public RelayCommand ClearCommand { get; set; }
+
+        public RelayCommand SendCommand { get; set; }
+
+        public RelayCommand AutoCompleteCommand { get; set; }
+
+        public RelayCommand SettingsCommand { get; set; }
+
+        public RelayCommand UserlistCommand { get; set; }
+
+        public RelayCommand LoginCommand { get; set; }
+
+        public RelayCommand LogoutCommand { get; set; }
+
+        public RelayCommand ShowLoginCommand { get; set; }
+
+        public RelayCommand SaveCommand { get; set; }
+
+        public RelayCommand RetrieveDetailsCommand { get; set; }
 
         public RelayCommand TrayCommand { get; set; }
 
+        public RelayCommand PreviousMessageCommand { get; set; }
+
+        public RelayCommand NextMessageCommand { get; set; }
+
+        public RelayCommand EmotesCommand { get; set; }
+
+        public RelayCommand<Emote> UseEmoteCommand { get; set; }
+
+        public RelayCommand ClosePopupCommand { get; set; }
+
+        // Method logic
+
+        private void PreviousMessage()
+        {
+            if (_commandHistoryIndex - 1 >= 0)
+                Message = _commandHistory[--_commandHistoryIndex];
+        }
+
+        private void NextMessage()
+        {
+
+            if (_commandHistoryIndex >= _commandHistory.Count - 1)
+            {
+                Message = "";
+                if (_commandHistoryIndex == _commandHistory.Count)
+                    _commandHistoryIndex++;
+            }
+            else Message = _commandHistory[++_commandHistoryIndex];
+        }
+
+        private void AutoComplete()
+        {
+            // I need some more weighting for active users
+            if (Message?.Trim().Length > 0)
+            {
+                var sentence = Message.Split(' ').ToList();
+
+                var word = sentence.Last();
+
+                sentence.RemoveAt(sentence.Count - 1);
+
+                var names = _chatViewModel.ActiveUsers.Where(user => user.ToLower().StartsWith(word.ToLower()))
+                    .ToList();
+
+                // BAD
+                if (!names.Any())
+                    names = ChatService.Users
+                        .Where(user => user.Username.ToLower().StartsWith(word.ToLower()))
+                        .Select(user => user.Username)
+                        .ToList();
+
+                var emotes = EmoteRepository.Emotes
+                    .Where(e => e.ToString().ToLower().StartsWith(word.ToLower()))
+                    .Select(e => e.ToString());
+
+                var result = names.Concat(emotes)
+                    .OrderBy(thing => Distance.LevenshteinDistance(thing.ToLower(), word.ToLower()))
+                    .FirstOrDefault();
+
+                if (result != null)
+                {
+                    if (sentence.Count >= 1)
+                        Message = string.Join(" ", sentence) + " " + result;
+                    else
+                        Message = result + " ";
+                }
+            }
+        }
+
+        private async void Logout()
+        {
+            _settingsRepository.Username = "";
+            _settingsRepository.Sid = "";
+            _settingsRepository.RememberMe = "";
+            LoggedIn = false;
+            await SettingsRepository.Save();
+        }
+
+        private async void Login()
+        {
+            if (_settingsRepository.Username.Length == 0)
+            {
+                (var successful, var username) = await ChatService.GetUsername();
+                if (successful)
+                {
+                    _settingsRepository.Username = username;
+                    ChatService.Login(SettingsRepository.Sid, SettingsRepository.RememberMe);
+                    LoggedIn = true;
+                }
+            }
+            await SettingsRepository.Save();
+        }
+
+        private void SendMessage()
+        {
+            if (!string.IsNullOrEmpty(Message))
+            {
+                ChatService.SendMessage(Message);
+                _commandHistory.Add(Message);
+                _commandHistoryIndex = _commandHistory.Count;
+            }
+            Message = "";
+        }
     }
 }
