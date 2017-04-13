fa4df1048a4ccafedddcda30799c11cbcfb69b6e
dukemiller <dukemiller928@hotmail.com>
2017-04-13 02:23:20 -0700

    Add autoscroll chat and initial binding behavior

---
+++ destiny-chat-client/Classes/Behaviors.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace destiny_chat_client.Classes
+{
+    // http://stackoverflow.com/questions/1977929/wpf-listbox-with-a-listbox-ui-virtualization-and-scrolling
+    public static class PixelBasedScrollingBehavior
+    {
+        public static bool GetIsEnabled(DependencyObject obj) => (bool)obj.GetValue(IsEnabledProperty);
+
+        public static void SetIsEnabled(DependencyObject obj, bool value) => obj.SetValue(IsEnabledProperty, value);
+
+        public static readonly DependencyProperty IsEnabledProperty = DependencyProperty.RegisterAttached("IsEnabled", typeof(bool), typeof(PixelBasedScrollingBehavior), new UIPropertyMetadata(false, HandleIsEnabledChanged));
+
+        private static void HandleIsEnabledChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var vsp = d as VirtualizingStackPanel;
+
+            if (vsp == null)
+                return;
+
+            var property = typeof(VirtualizingStackPanel).GetProperty("IsPixelBased",
+                BindingFlags.NonPublic | BindingFlags.Instance);
+
+            if (property == null)
+                throw new InvalidOperationException("Pixel-based scrolling behaviour hack no longer works!");
+
+            property.SetValue(vsp, (bool) e.NewValue, new object[0]);
+        }
+    }
+}
--- destiny-chat-client/Views/Components/Chat.xaml.cs
+++ destiny-chat-client/Views/Components/Chat.xaml.cs
@@ -20,9 +20,92 @@ namespace destiny_chat_client.Views.Components
     /// </summary>
     public partial class Chat : UserControl
     {
+        public static bool AutoScroll = true;
+
+        public static bool NewUnreadMessages;
+
+        public static bool UserScrolled;
+
+        public static bool ViewAtBottom;
+
+        private readonly ScrollViewer _scrollViewer;
+
         public Chat()
         {
             InitializeComponent();
+            _scrollViewer = (ScrollViewer)GetDescendantByType(this, typeof(ScrollViewer));
+            _scrollViewer.ScrollChanged += ScrollViewer_OnScrollChanged;
+        }
+
+        private void ListBoxItem_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                var lbi = sender as ListBoxItem;
+                if (lbi != null)
+                {
+                    lbi.IsSelected = true;
+                    lbi.Focus();
+                    // MessageBox.Show("WEHAWE");
+                    // lb.SelectedItems.Add(lbi);
+                }
+            }
+        }
+
+        // http://stackoverflow.com/questions/16683477/change-scrollviewer-template-in-listbox
+        // http://stackoverflow.com/questions/2984803/how-to-automatically-scroll-scrollviewer-only-if-the-user-did-not-change-scrol
+        private void ScrollViewer_OnScrollChanged(object sender, ScrollChangedEventArgs e)
+        {
+            NewUnreadMessages = Math.Abs(e.ExtentHeightChange) > 0.1;
+            UserScrolled = Math.Abs(e.ExtentHeightChange) < 0.1;
+            ViewAtBottom = Math.Abs(_scrollViewer.VerticalOffset - _scrollViewer.ScrollableHeight) < 0.1;
+
+            if (UserScrolled)
+            {
+                if (ViewAtBottom)
+                {
+                    NewUnreadMessages = false;
+                    MoreMessagesPane.Visibility = Visibility.Collapsed;
+                }
+                AutoScroll = ViewAtBottom;
+            }
+
+            if (AutoScroll && NewUnreadMessages)
+                _scrollViewer.ScrollToVerticalOffset(_scrollViewer.ExtentHeight);
+
+            if (NewUnreadMessages)
+                MoreMessagesPane.Visibility = ViewAtBottom ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        // http://stackoverflow.com/questions/10293236/accessing-the-scrollviewer-of-a-listbox-from-c-sharp
+        public static Visual GetDescendantByType(Visual element, Type type)
+        {
+            if (element == null)
+                return null;
+
+            if (element.GetType() == type)
+                return element;
+
+            Visual foundElement = null;
+
+            (element as FrameworkElement)?.ApplyTemplate();
+
+            for (var i = 0; i < VisualTreeHelper.GetChildrenCount(element); i++)
+            {
+                var visual = VisualTreeHelper.GetChild(element, i) as Visual;
+                foundElement = GetDescendantByType(visual, type);
+                if (foundElement != null)
+                    break;
+            }
+
+            return foundElement;
+        }
+
+        private void GoToBottom(object sender, MouseButtonEventArgs e)
+        {
+            MoreMessagesPane.Visibility = Visibility.Collapsed;
+            AutoScroll = true;
+            _scrollViewer.ScrollToVerticalOffset(_scrollViewer.ExtentHeight);
         }
     }
 }
